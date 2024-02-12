09dbfb31dbe187ff8b3072f0aab1ca6b768f3a00
TadHittle <69868209+TadHittle@users.noreply.github.com>
2024-02-12 11:27:26 -0700

    Add files via upload

---
+++ Week 3 - 2.14 Exercise 1 - Hittle.py
@@ -0,0 +1,14 @@
+"defined variables below"
+a = "All"
+b = "work"
+c = "and"
+d = "no"
+e = "play"
+f = "makes"
+g = "jack"
+h = "a"
+i = "dull"
+j = "boy."
+
+"Program"
+print (a,b,c,d,e,f,g,h,i,j)
